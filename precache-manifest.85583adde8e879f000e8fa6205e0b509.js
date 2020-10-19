a14cce0e750be7d4eacb7a95ccfe9fcd5d44ce55
Leandro Guaraldi <leandro_319@yahoo.com.br>
2020-10-19 16:46:21 -0300

    Updates

---
+++ precache-manifest.85583adde8e879f000e8fa6205e0b509.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "698a8417d9ffc9a83a198ac658e886c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1db7c6918ec656607ab1",
+    "url": "/static/css/main.2346fb2c.chunk.css"
+  },
+  {
+    "revision": "2aeae9788a79c1bdbf10",
+    "url": "/static/js/2.b0ae1d37.chunk.js"
+  },
+  {
+    "revision": "7585d7e2191ead1c7fd01d7ebe90ade7",
+    "url": "/static/js/2.b0ae1d37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1db7c6918ec656607ab1",
+    "url": "/static/js/main.238a5cee.chunk.js"
+  },
+  {
+    "revision": "1e967e60c8804a474105",
+    "url": "/static/js/runtime-main.9050acbe.js"
+  },
+  {
+    "revision": "924def97be2037b56e28772ea874088a",
+    "url": "/static/media/foto.924def97.png"
+  },
+  {
+    "revision": "acf5c70e15798e626262aa5218854660",
+    "url": "/static/media/iconDevPhp.acf5c70e.png"
+  },
+  {
+    "revision": "78e4bcf2348898befa489073f1b5685e",
+    "url": "/static/media/iconsQualidadesCss.78e4bcf2.png"
+  },
+  {
+    "revision": "d639b7370ad33f836bd965ece58de09b",
+    "url": "/static/media/iconsQualidadesWp.d639b737.png"
+  },
+  {
+    "revision": "4a6d3773802b626be627ea5276a8028f",
+    "url": "/static/media/propharmacos.4a6d3773.png"
+  },
+  {
+    "revision": "35702041595bbe692255420e395cbd88",
+    "url": "/static/media/sheila.35702041.png"
+  },
+  {
+    "revision": "081791728283d89d2a3b09ccf4938d58",
+    "url": "/static/media/simplez.08179172.png"
+  },
+  {
+    "revision": "4b1e72d785ed119acea22905528c638d",
+    "url": "/static/media/vitoria.4b1e72d7.png"
+  }
+]);
