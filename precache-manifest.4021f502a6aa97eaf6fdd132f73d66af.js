751f84bdf7ae6519cc061d20ad768ab4cc5d8636
Leandro Guaraldi <leandro_319@yahoo.com.br>
2020-10-19 16:48:03 -0300

    Updates

---
+++ precache-manifest.4021f502a6aa97eaf6fdd132f73d66af.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4f6e5173919a7a394907311ae41b6e4",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "1db7c6918ec656607ab1",
+    "url": "/portfolio/static/css/main.2346fb2c.chunk.css"
+  },
+  {
+    "revision": "98e0217d7dacaf71bec9",
+    "url": "/portfolio/static/js/2.5c4dbfae.chunk.js"
+  },
+  {
+    "revision": "7585d7e2191ead1c7fd01d7ebe90ade7",
+    "url": "/portfolio/static/js/2.5c4dbfae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1db7c6918ec656607ab1",
+    "url": "/portfolio/static/js/main.238a5cee.chunk.js"
+  },
+  {
+    "revision": "fb81100604e786b60a00",
+    "url": "/portfolio/static/js/runtime-main.c525fe00.js"
+  },
+  {
+    "revision": "924def97be2037b56e28772ea874088a",
+    "url": "/portfolio/static/media/foto.924def97.png"
+  },
+  {
+    "revision": "acf5c70e15798e626262aa5218854660",
+    "url": "/portfolio/static/media/iconDevPhp.acf5c70e.png"
+  },
+  {
+    "revision": "78e4bcf2348898befa489073f1b5685e",
+    "url": "/portfolio/static/media/iconsQualidadesCss.78e4bcf2.png"
+  },
+  {
+    "revision": "d639b7370ad33f836bd965ece58de09b",
+    "url": "/portfolio/static/media/iconsQualidadesWp.d639b737.png"
+  },
+  {
+    "revision": "4a6d3773802b626be627ea5276a8028f",
+    "url": "/portfolio/static/media/propharmacos.4a6d3773.png"
+  },
+  {
+    "revision": "35702041595bbe692255420e395cbd88",
+    "url": "/portfolio/static/media/sheila.35702041.png"
+  },
+  {
+    "revision": "081791728283d89d2a3b09ccf4938d58",
+    "url": "/portfolio/static/media/simplez.08179172.png"
+  },
+  {
+    "revision": "4b1e72d785ed119acea22905528c638d",
+    "url": "/portfolio/static/media/vitoria.4b1e72d7.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.85583adde8e879f000e8fa6205e0b509.js"
+  "/portfolio/precache-manifest.4021f502a6aa97eaf6fdd132f73d66af.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
